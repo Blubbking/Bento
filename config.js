f3d4cd01cf8a8942386d6be8988ee3bb27482e34
Blubbking <101980674+Blubbking@users.noreply.github.com>
2022-03-20 15:23:45 +0100

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ const CONFIG = {
 	greetingNight: 'You should rest',
 
 	// Layout
-	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+	bentoLayout: 'buttons', // 'bento', 'lists', 'buttons'
 
 	// Weather
 	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
